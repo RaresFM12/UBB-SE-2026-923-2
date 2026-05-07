ef7de13b582d3f1325e2daf1bd6501fca4fa46f1
PaulFulop <paul.fulop@stud.ubbcluj.ro>
2026-05-08 00:51:26 +0300

    latest changes after publish..

--- UBB-SE-2026-923-2/UBB-SE-2026-923-2.WebApi/Program.cs
+++ UBB-SE-2026-923-2/UBB-SE-2026-923-2.WebApi/Program.cs
@@ -60,11 +60,8 @@ builder.Services.AddScoped<ISubstancesRepository, SQLSubstancesRepository>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
