9cb8fa34000bbdaddd2c229932a8ac3cf931692d
dorufloare <dorufloare@yahoo.com>
2026-05-18 21:16:26 +0300

    bugfix

--- UBB-SE-2026-923-2/UBB-SE-2026-923-2.Web/Models/DoctorOptionViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace UBB_SE_2026_923_2.Web.Models
-{
-    public class DoctorOptionViewModel
-    {
-        public int DoctorId { get; set; }
-
-        public string DoctorName { get; set; } = string.Empty;
-    }
-}
